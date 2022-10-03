5ec22a95428c87396b62016ba843433f52752526
Adikaraalff <73709213+Adikaraalff@users.noreply.github.com>
2022-10-03 11:20:30 +0700

    update

--- app/src/main/java/com/if3b/pindah_layout/MainActivity.java
+++ app/src/main/java/com/if3b/pindah_layout/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnpindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Layout A");
+
         btnpindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
 
         btnpindah.setOnClickListener(new View.OnClickListener() {
             @Override
